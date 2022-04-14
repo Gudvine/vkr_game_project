9db3da95d74c9a54faaef1c87d3d994bd854ef7c
Gudvine <karapuz719@gmail.com>
2022-04-15 01:24:16 +0300

    added camera, bg and etc.

---
+++ game_proj/scenes/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const SPEED = 100.0
+const FLOOR = Vector2(0, -1)
+const GRAVITY = 950
+const JUMP_POWER = 350
+
+var velocity_value = Vector2()
+
+func _physics_process(delta):
+	
+	if position.y > 800:
+		position.y = 0
+	
+	if Input.is_action_pressed("ui_left"):
+		$Sprite.flip_h = true
+		velocity_value.x = -SPEED
+	elif Input.is_action_pressed("ui_right"):
+		$Sprite.flip_h = false
+		velocity_value.x = SPEED
+	else:
+		velocity_value.x = 0
+	
+	if Input.is_action_pressed("ui_up") && is_on_floor():
+		velocity_value.y = -JUMP_POWER
+
+	velocity_value.y += GRAVITY * delta
+	velocity_value = move_and_slide(velocity_value, FLOOR)  
